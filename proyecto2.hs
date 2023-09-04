b74223f0d300f6eba0ff61d0464d6d2c23cf402d
serenaOlivera <142268373+serenaOlivera@users.noreply.github.com>
2023-09-04 12:54:17 -0300

    Add files via upload

--- proyecto2.hs
+++ proyecto2.hs
@@ -28,7 +28,9 @@ titulo Computacion = "Licenciatura en Computacion"
 titulo Astronomia = "Licenciatura en Astronomia"
 
 --c)
-data NotaBasica = Do | Re | Mi | Fa | Sol | La | Si
+data NotaBasica = Do | Re | Mi 
+                | Fa | Sol | La | Si 
+                {-Ejerc2.a-} deriving (Eq, Ord, Show)
 
 --d)
 cifradoAmericano :: NotaBasica -> Char
@@ -39,4 +41,42 @@ cifradoAmericano Fa = 'D'
 cifradoAmericano Sol = 'E'
 cifradoAmericano La = 'F'
 cifradoAmericano Si = 'G'
+
+{-Ejercicio 2
+a) Completar la definicion del tipo NotaBasica para que las expresiones
+*Main> Do <= Re
+*Main> Fa `min` Sol
+sean validas y no generen error. Ayuda: usar deriving con multiples clases.
+-}
+
+{-Ejercicio 3
+a) Definir usando polimorfismo ad hoc la funcion minimoElemento que calcula (de manera
+recursiva) cual es el menor valor de una lista de tipo [a]. Asegurarse que solo este
+definida para listas no vacıas.
+b) Definir la funcion minimoElemento’ de manera tal que el caso base de la recursion
+sea el de la lista vacıa. Para ello revisar la clase Bounded.
+Ayuda: Para probar esta funcion dentro de ghci con listas vacıas, indicar el tipo concre-
+to con tipos de la clase Bounded, por ejemplo: ([1,5,10]::[Int]), ([]::[Bool]),
+etc.
+c) Usar la funcion minimoElemento para determinar la nota mas grave de la melodıa:
+[Fa, La, Sol, Re, Fa]
+-}
+
+--a)
+minimoElemento :: (Eq (a), Ord (a), Show (a)) => [a] -> a 
+minimoElemento [g] = g
+minimoElemento (g:h:gs) =  g `min` minimoElemento (h:gs)  
+
+--b)
+minimoElemento' :: (Eq (a), Ord (a), Show (a), Bounded (a)) => [a] -> a
+minimoElemento' [] = minBound 
+minimoElemento' (g:gs)  = g `min` minimoElemento' gs
+
+--c)
+graveNota :: [NotaBasica] -> NotaBasica
+graveNota [g] = g  
+graveNota (g:gs) = g `min` graveNota gs  
+
+
+
 
