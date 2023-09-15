f85c72920ef7df0e8df88710e521aa2dab72df34
serenaOlivera <142268373+serenaOlivera@users.noreply.github.com>
2023-09-15 17:23:23 -0300

    Add files via upload

--- proyecto2.hs
+++ proyecto2.hs
@@ -1,6 +1,6 @@
-{-Preguntar:
-Ejercicio 3 c ¿Como defino a Show para que me ande?
--}
+-- Serena Olivera
+
+-- fijarse 7) 3.
 
 {- Ejercicio 1
 data Carrera = Matematica | Fisica | Computacion | Astronomia
@@ -42,7 +42,7 @@ ghci> titulo Fisica
 --c)
 data NotaBasica = Do | Re | Mi 
                 | Fa | Sol | La | Si 
-                {-Ejerc2.a-} deriving (Eq, Ord, Show)
+                {-Ejerc2.a-} deriving (Show, Ord, Eq)
 
 --d)
 cifradoAmericano :: NotaBasica -> Char
@@ -178,18 +178,13 @@ ghci> contarVelocistas []
 
 contar_futbolistas :: [Deportista] -> Zona -> Int
 contar_futbolistas [] z = 0
-
-{-
-contar_futbolistas (g:gs) z = case g of 
-                                Futbolista z _ _ _ -> 1 + contar_futbolistas gs z
-                                _  -> contar_futbolistas gs z
--}
                                
 contar_futbolistas (Futbolista Arco n p a : gs) Arco = 1 + contar_futbolistas gs Arco
 contar_futbolistas (Futbolista Defensa n p a : gs) Defensa = 1 + contar_futbolistas gs Defensa
 contar_futbolistas (Futbolista Mediocampo n p a : gs) Mediocampo = 1 + contar_futbolistas gs Mediocampo
 contar_futbolistas (Futbolista Delantera n p a : gs) Delantera = 1 + contar_futbolistas gs Delantera
 contar_futbolistas ( _ : gs) z = contar_futbolistas gs z
+
 
 --e) mi funcion no usa filter 
 
@@ -198,10 +193,177 @@ zonaFiltrada (Futbolista s n p a) z | s == z = True
                                     | s /= z = False
 zonaFiltrada ( _ ) z = False 
 
+{-Ejemplos:
+ghci> zonaFiltrada Ajedrecista Mediocampo
+False
+ghci> zonaFiltrada (Futbolista Delantera 5 Derecha 120) Mediocampo 
+False
+ghci> zonaFiltrada (Futbolista Mediocampo 5 Derecha 120) Mediocampo 
+True
+-}
 
 {-
-contar_futbolistas' :: [Deportista] -> Zona -> Int
-contar_futbolistas' [] z = 0
-contar_futbolistas' 
+contarFutbolistas' :: [Deportista] -> Zona -> Int
+contarFutbolistas' [] z = 0
+contarFutbolistas' (g:gs) z = filter zonaFiltrada (contar_futbolistas' gs z)
 -}
- 
+
+-- Ejercicio 5
+{-a) Implementa la funcion sonidoNatural como esta definida arriba.
+b) Definir el tipo enumerado Alteracion que consta de los constructores Bemol, Natural
+y Sostenido.
+c) Definir el tipo algebraico NotaMusical que debe tener un solo un constructor que llamaremos Nota el cual toma
+dos parametros. El primer parametro es de tipo NotaBasica
+y el segundo de tipo Alteracion. De esta manera cuando se quiera representar una
+nota alterada se puede usar como segundo parametro del constructor un Bemol o
+Sostenido y si se quiere representar una nota sin alteraciones se usa Natural como
+segundo parametro.
+d) Definı la funcion sonidoCromatico :: NotaMusical -> Int que devuelve el sonido
+de una nota, incrementando en uno su valor si tiene la alteracion Sostenido,
+decrementando en uno si tiene la alteracion Bemol y dejando su valor intacto si la alteracion
+es Natural
+e) Incluı el tipo NotaMusical a la clase Eq de manera tal que dos notas que tengan el
+mismo valor de sonidoCromatico se consideren iguales.
+f ) Incluı el tipo NotaMusical a la clase Ord definiendo el operador <=. Se debe definir
+que una nota es menor o igual a otra si y solo si el valor de sonidoCromatico para la
+primera es menor o igual al valor de sonidoCromatico para la segunda
+-}
+
+-- a)
+sonidoNatural :: NotaBasica -> Int
+sonidoNatural Do = 0
+sonidoNatural Re = 2
+sonidoNatural Mi = 4
+sonidoNatural Fa = 5
+sonidoNatural Sol = 7
+sonidoNatural La = 9
+sonidoNatural Si = 11
+
+-- b) 
+data Alteracion = Bemol | Natural | Sostenido deriving (Eq, Ord)
+
+
+-- c) 
+data NotaMusical = Nota  NotaBasica Alteracion  deriving (Eq, Ord)
+
+-- d)
+sonidoAlteracion :: Alteracion -> Int
+sonidoAlteracion Bemol = (-1)
+sonidoAlteracion Natural = 0
+sonidoAlteracion Sostenido = 1
+
+
+sonidoCromatico :: NotaMusical -> Int
+sonidoCromatico (Nota n a) = sonidoNatural n + sonidoAlteracion a
+
+{- Ejemplos:
+ghci> sonidoCromatico (Nota Do Bemol)
+-1
+ghci> sonidoCromatico (Nota Do Sostenido)
+1
+ghci> sonidoCromatico (Nota Fa Sostenido)
+6
+ghci> sonidoCromatico (Nota Fa Bemol)    
+4
+-}
+
+-- e) 
+{-Ejemplos
+ghci> sonidoCromatico (Nota Fa Sostenido) == sonidoCromatico (Nota Sol Bemol)    
+True
+ghci> sonidoCromatico (Nota Si Bemol) == sonidoCromatico (Nota La Sostenido) 
+True
+-}
+
+-- f)
+{-Ejemplos:
+ghci> (Nota Do Bemol) <= (Nota Do Natural) 
+True
+ghci> (Nota Do Bemol) <= (Nota Fa Natural) 
+True
+ghci> (Nota Si Bemol) <= (Nota Fa Natural) 
+False
+ghci> sonidoCromatico (Nota Si Bemol) <= sonidoCromatico (Nota Fa Natural)
+False
+ghci> sonidoCromatico (Nota Do Bemol) <= sonidoCromatico (Nota Do Natural) 
+True
+-}
+
+-- Ejercicio 6 
+{-a) Definir la funcion primerElemento que devuelve el primer elemento de una lista no
+vacıa, o Nothing si la lista es vacıa.
+primerElemento : : [ a ] −> Maybe a
+-}
+
+-- a)
+primerElemento :: [a] -> Maybe a
+primerElemento [] = Nothing
+primerElemento (g:gs) = Just g 
+
+{- Ejemplos:
+ghci> primerElemento []
+Nothing
+ghci> primerElemento [1,2,3,4] 
+Just 1
+ghci> primerElemento [True, False, True, False]
+Just True
+-}
+
+-- Ejercicio 7
+
+{- a) Programa las siguientes funciones:
+1) atender :: Cola -> Maybe Cola, que elimina de la cola a la persona que esta
+en la primer posicion de una cola, por haber sido atendida. Si la cola esta vacıa,
+devuelve Nothing.
+2) encolar :: Deportista -> Cola -> Cola, que agrega una persona a una cola
+de deportistas, en la ultima posicion.
+3) busca :: Cola -> Zona -> Maybe Deportista, que devuelve el/la primera
+futbolista dentro de la cola que juega en la zona que se corresponde con el segundo
+parametro. Si no hay futbolistas jugando en esa zona devuelve Nothing.
+b) ¿A que otro tipo se parece Cola?.
+-}
+
+data Cola = VaciaC | Encolada Deportista Cola deriving Show
+
+-- a)
+-- 1.
+atender :: Cola -> Maybe Cola
+atender g = case g of
+                VaciaC -> Nothing
+                Encolada d c -> Just c 
+
+{-Ejemplos:
+ghci> atender (Encolada Ajedrecista (Encolada (Velocista 120) (VaciaC)))
+Just (Encolada (Velocista 120) VaciaC)
+ghci> atender (Encolada Ajedrecista (Encolada (Velocista 40) (Encolada (Ciclista Monte) VaciaC)))
+Just (Encolada (Velocista 40) (Encolada (Ciclista Monte) VaciaC))
+-}
+
+-- 2.
+encolar :: Deportista -> Cola -> Cola
+encolar d VaciaC = Encolada d VaciaC
+encolar d (Encolada d' c) = Encolada d' (encolar d c)
+
+{-Ejemplos:
+ghci> encolar Ajedrecista (Encolada (Velocista 40) VaciaC)
+Encolada (Velocista 40) (Encolada Ajedrecista VaciaC)
+ghci> encolar (Ciclista Monte) (Encolada Ajedrecista (Encolada (Velocista 40) VaciaC))
+Encolada Ajedrecista (Encolada (Velocista 40) (Encolada (Ciclista Monte) VaciaC))
+-}
+
+-- 3.
+busca :: Cola -> Zona -> Maybe Deportista
+busca VaciaC z = Nothing
+busca (Encolada d c) z = case d of
+                        Futbolista z _ _ _ -> Just d
+                        _ -> busca VaciaC z
+
+{- ghci> busca (Encolada (Futbolista Defensa 5 Derecha 170) VaciaC) Arco
+Just (Futbolista Defensa 5 Derecha 170)
+PORQUE AAAAAAAAAAAAAAA
+-}
+
+-- b)
+{-Cola se parece al tipo de lista, que es lista = Vacía | Agregar Int lista-}
+
+-- Ejercicio 8 
